87f8d634b48a0a83e58f83dcda19855bdb02f10c
imane <imanezenasnii@gmail.com>
2025-01-25 15:21:30 +0100

    Added user schema with different fields

---
+++ backend/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  name: { type: String, trim: true, required: true },
+  email: {
+    type: String, trim: true, required: true, unique: true,
+  },
+  password: { type: String, required: true },
+  isVerified: { type: Boolean, required: true, default: false },
+
+}, { versionKey: false });
+
+module.exports = mongoose.model('User', userSchema);
