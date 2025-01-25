acaea695ddd020933288729c0bd935c45969529e
imane <imanezenasnii@gmail.com>
2025-01-25 17:26:37 +0100

    Add user router with POST /create endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+
 require('./db');
 
 const app = express();
-
 app.use(express.json());
+
+const userRouter = require('./routes/user');
+
+app.use('/api/user', userRouter);
 
 const PORT = 4000;
 app.listen(PORT, () => {
