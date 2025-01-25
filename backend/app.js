e0933bcfca461332860cc7fdf09da1a2b8b9dce0
imane <imanezenasnii@gmail.com>
2025-01-25 18:34:55 +0100

    Use morgan middleware to log HTTP request

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
-
+const morgan = require('morgan');
 require('./db');
 
 const app = express();
 app.use(express.json());
-
+app.use(morgan('dev'));
 const userRouter = require('./routes/user');
-
 app.use('/api/user', userRouter);
 
 const PORT = 4000;
