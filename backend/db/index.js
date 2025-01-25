fded0b632db9015048bd2f5af36f8f268c3ba4e5
imane <imanezenasnii@gmail.com>
2025-01-25 14:37:27 +0100

    Implement MongoDB connection using Mongoose

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-// Connect to MongoDB database 'myDb' using Mongoose
-mongoose.connect('mongodb://localhost:27017/myDb', {}).then(() => {
+// Connect to MongoDB database 'Db' using Mongoose
+mongoose.connect('mongodb://localhost:27017/Db', {}).then(() => {
   console.log('DB is connected');
 }).catch((err) => {
   console.log(err);
