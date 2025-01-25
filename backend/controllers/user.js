b33ca039915e0770e2039f0fd626cfb573cdc7b1
imane <imanezenasnii@gmail.com>
2025-01-25 21:41:01 +0100

    create mail transporter for sending emails using Mailtrap

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,7 @@
 const User = require('../models/user');
+const EmailVerificationToken = require('../models/emailVerificationToken');
+const { generateOTP, generateMailTransporter } = require('../utils/mail');
 const { sendError } = require('../utils/helper');
-
-// Controller to create a new user
 
 exports.create = async (req, res) => {
   const { name, email, password } = req.body;
@@ -18,6 +18,21 @@ exports.create = async (req, res) => {
   const newEmailVerificationToken = new EmailVerificationToken({ owner: newUser._id, token: OTP });
 
   await newEmailVerificationToken.save();
+
+  // Send email with OTP for user email verification
+  const transport = generateMailTransporter();
+
+  transport.sendMail({
+    from: 'no-reply@cinestream.com',
+    to: newUser.email,
+    subject: 'Email Verification',
+    html: `
+    <div style="border:1px; width:87%;box-shadow: 0px 4px 8px silver;padding:10px; margin-left:auto; margin-right:auto;text-align:center">
+    <h2>Your verification OTP</h2>
+    <h2 style="color:salmon">${OTP}</h2>
+    </div>
+    `,
+  });
 
   return res.status(201).json({
     user: {
--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -1,10 +1,20 @@
+const nodemailer = require('nodemailer');
 // Generate a random OTP
-exports.generateOTP = (otp_length = 6) => {
-    let OTP = '';
-    for (let i = 1; i <= otp_length; i += 1) {
-      const randomVal = Math.round(Math.random() * 9);
-      OTP += randomVal;
-    }
-    return OTP;
-  };
-  
+exports.generateOTP = (otpLength = 6) => {
+  let OTP = '';
+  for (let i = 1; i <= otpLength; i += 1) {
+    const randomVal = Math.round(Math.random() * 9);
+    OTP += randomVal;
+  }
+  return OTP;
+};
+
+//  mail transporter for sending emails using Mailtrap
+exports.generateMailTransporter = () => nodemailer.createTransport({
+  host: 'sandbox.smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: process.env.MAIL_TRAP_USER,
+    pass: process.env.MAIL_TRAP_PASSWORD,
+  },
+});
