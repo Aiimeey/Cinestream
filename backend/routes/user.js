8691edf5b58496254d418b3b63284ef14c054515
imane <imanezenasnii@gmail.com>
2025-01-26 02:34:03 +0100

    Define routes for /sign-in

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { create } = require('../controllers/user');
-const { userValidator, validate } = require('../middlewares/validator');
+const { create, verifyEmail, signIn } = require('../controllers/user');
+const { userValidator, validate, signInValidator } = require('../middlewares/validator');
 
 const router = express.Router();
 
 router.post('/create', userValidator, validate, create);
 router.post('/verify-email', verifyEmail);
+router.post('/sign-in', signInValidator, validate, signIn);
 
 module.exports = router;
