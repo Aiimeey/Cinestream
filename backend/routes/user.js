ea78620511333e3e131b8dadcf151cfec0616833
imane <imanezenasnii@gmail.com>
2025-01-26 20:21:23 +0100

    Define forget password route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {
   create, verifyEmail, signIn, resendEmailVerificationToken,
+  forgetPassword,
 } = require('../controllers/user');
 const { userValidator, validate, signInValidator } = require('../middlewares/validator');
 
@@ -10,5 +11,5 @@ router.post('/create', userValidator, validate, create);
 router.post('/verify-email', verifyEmail);
 router.post('/sign-in', signInValidator, validate, signIn);
 router.post('/resend-email-verification-token', resendEmailVerificationToken);
-
+router.post('/forget-password', forgetPassword);
 module.exports = router;
