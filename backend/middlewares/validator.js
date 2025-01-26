d7652791b7e8295ebf784efd4bcc57f5dbf7fff9
imane <imanezenasnii@gmail.com>
2025-01-26 02:32:55 +0100

    Add signin validator for email and password

--- backend/middlewares/validator.js
+++ backend/middlewares/validator.js
@@ -9,9 +9,16 @@ exports.userValidator = [
     .isLength({ min: 8, max: 20 })
     .withMessage('Password must be 8 to 20 characters long'),
 ];
-// Middleware to handle validation errors
+
+// gathering all the validation errors from the req & putting them into an array called error
 exports.validate = (req, res, next) => {
   const error = validationResult(req).array();
   if (error.length) return res.json({ error: error[0].msg });
-  next();
+  return next();
 };
+
+exports.signInValidator = [
+  check('email').normalizeEmail().isEmail().withMessage('Email is invalid!'),
+  check('password').trim().not().isEmpty()
+    .withMessage('Password is missing!'),
+];
