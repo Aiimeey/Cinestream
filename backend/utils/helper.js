00e50165d0ae6a7400a7cf3da92ba29d49556585
imane <imanezenasnii@gmail.com>
2025-01-26 17:45:16 +0100

    Implement generateRandomByte method for generating  random 30-byte hexadecimal string

--- backend/utils/helper.js
+++ backend/utils/helper.js
@@ -1,3 +1,14 @@
+const crypto = require('crypto');
+
+// Sends a standardized error response with a custom status code
 exports.sendError = (res, error, statusCode = 401) => {
   res.status(statusCode).json({ error });
 };
+// Generate random 30-byte hexadecimal string
+exports.generateRandomByte = () => new Promise((resolve, reject) => {
+  crypto.randomBytes(30, (err, randomByte) => {
+    if (err) reject(err);
+    const data = randomByte.toString('hex');
+    resolve(data);
+  });
+});
