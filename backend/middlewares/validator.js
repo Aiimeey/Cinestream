0412e0979f378e6a135354c87d48fe3ac6896240
imane <imanezenasnii@gmail.com>
2025-01-27 11:00:28 +0100

    Add express-validator for new password validation

--- backend/middlewares/validator.js
+++ backend/middlewares/validator.js
@@ -1,5 +1,5 @@
 const { check, validationResult } = require('express-validator');
-// Validator for user
+// Validator for user ( express-validator has its own in-memory store to track the errors)
 exports.userValidator = [
   check('name').trim().not().isEmpty()
     .withMessage('name is missing'),
@@ -21,4 +21,14 @@ exports.signInValidator = [
   check('email').normalizeEmail().isEmail().withMessage('Email is invalid!'),
   check('password').trim().not().isEmpty()
     .withMessage('Password is missing!'),
+];
+
+exports.validatePassword = [
+  check('newPassword')
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage('Password is missing!')
+    .isLength({ min: 8, max: 20 })
+    .withMessage('Password must be 8 to 20 characters long!'),
 ];
