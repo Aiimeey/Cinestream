9b5cde7c4f127271b540c5ebb7f77aa87c11466c
imane <imanezenasnii@gmail.com>
2025-01-28 06:26:24 +0100

    Enable cors middleware

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 require('express-async-errors');
 require('./db');
+const cors = require('cors');
 const { errorHandler } = require('./middlewares/error');
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 const userRouter = require('./routes/user');
 
+app.use(cors());
 app.use('/api/user', userRouter);
 app.use(errorHandler);
 const PORT = 4000;
