b4d3fbb72da4cc96f4f10fc8354ad546a8db750c
imane <imanezenasnii@gmail.com>
2025-01-30 22:36:05 +0100

    initialize frontend project

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#171717",
+        secondary: "#F0EAD6",
+        "dark-subtle": "#b4b9c0",
+        "light-subtle": "rgba(39, 39, 39, 0.5)",
+        "off-white" :"#FAF9F6",
+        "linen" : "#E9DCC9",
+        "eggshell": "#ede8d5",
+        "bone-white":"#F9F6EE",
+        "alabaster":"#EDEADE",
+        "taupe":"#A89F91",
+        "navy":"#2E3A47",
+        "light-coral":"#db8170",
+        "light-terracotta":"df7a66",
+        "beige": "#f1ebd7"
+      },
+    },
+  },
+  plugins: [],
+};
