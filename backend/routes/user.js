5c02ae3b7872e475fb805be13972a0a9ebe019b2
imane <imanezenasnii@gmail.com>
2025-01-31 09:02:20 +0100

    Implement authentication middleware to verify JWT token and user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -9,6 +9,7 @@ const {
   userValidator, validate, signInValidator, validatePassword,
 } = require('../middlewares/validator');
 const { isValidPassResetToken } = require('../middlewares/user');
+const { isAuth } = require('../middlewares/auth');
 
 const router = express.Router();
 
@@ -19,5 +20,12 @@ router.post('/resend-email-verification-token', resendEmailVerificationToken);
 router.post('/forget-password', forgetPassword);
 router.post('/verify-pass-reset-token', isValidPassResetToken, sendResetPasswordTokenStatus);
 router.post('/reset-password', validatePassword, validate, isValidPassResetToken, resetPassword);
-
+router.get('/is-auth', isAuth, (req, res) => {
+  const { user } = req;
+  return res.json({
+    user: {
+      id: user._id, name: user.name, email: user.email, isVerified: user.isVerified,
+    },
+  });
+});
 module.exports = router;
