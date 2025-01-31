61dd6c866822553c43dea7ce9fea016456841b3c
imane <imanezenasnii@gmail.com>
2025-01-31 08:26:56 +0100

    Implement authentication context with login logout and token validation

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { NotificationContext } from "../context/NotificationProvider";
+import { AuthContext } from "../context/AuthProvider";
 
 export const useNotification = () => useContext(NotificationContext);
-
+export const useAuth = () => useContext(AuthContext);
