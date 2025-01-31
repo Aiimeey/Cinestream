5701800f45088543a0693177bc40c06a9bc9f699
imane <imanezenasnii@gmail.com>
2025-01-31 07:31:27 +0100

    Add NotFound component with 404 error image

---
+++ frontend/src/components/NotFound.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NotFound() {
+  return <div className="fixed inset-0 flex justify-center items-center -z-10"> <img src="../404-error.png" alt=" " className="w-auto h-auto" /></div>;
+}
